62372031ba405ed3863a184917ce865908d9f7d3
Kayck Matias <kayckmatias04@gmail.com>
2023-04-10 08:25:56 -0300

    [solved] 1098 - beginner

--- beginner/1098.php
+++ beginner/1098.php
@@ -12,10 +12,8 @@
 // Output
 // Print the sequence like the example below.
 
-//ATTENTION: This code works and the output is equal the uDebug of beecrowd, but beecrowd dont accept for some reason:c
-
 for ($i = 0; $i <= 2; $i += 0.2) {
     for ($j = 1; $j <= 3; $j++) {
-        echo "I=$i J=".$j+$i."\n";
+        echo "I=$i J=".($j+$i)."\n";
     }
 }
