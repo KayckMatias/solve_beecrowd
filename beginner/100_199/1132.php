193aa29d2d78098491cc357e5b94c8d95d2b4c09
Kayck <kayckmatias04@gmail.com>
2023-04-13 20:46:25 -0300

    [solved] 1132 - beginner

---
+++ beginner/100_199/1132.php
@@ -0,0 +1,29 @@
+<?php
+// beecrowd | 1132
+// Multiples of 13
+// Adapted by Neilor Tonin, URI  Brazil
+
+// Timelimit: 1
+// Write a program that reads two integer numbers X and Y and calculate the sum of all number not divisible by 13 between them, including both.
+
+// Input
+// The input file contains 2 integer numbers X and Y without any order.
+
+// Output
+// Print the sum of all numbers between X and Y not divisible by 13, including them if it is the case.
+
+$x = intval(fgets(STDIN));
+$y = intval(fgets(STDIN));
+
+$x = min($x,$y);
+$y = max($x,$y);
+
+$count = 0;
+
+for($i = $x; $i <= $y; $i++){
+    if($i % 13 != 0){
+        $count += $i;
+    }
+}
+
+echo $count . "\n";
